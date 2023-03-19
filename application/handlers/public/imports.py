f0e1117bcf49b403e07019ffcc51147181cd2178
Macbook Pro <macbookpro@Liams-MacBook-Pro.local>
2023-03-18 23:01:37 -0700

    improved import details page and tidied backend

--- application/handlers/public/imports.py
+++ application/handlers/public/imports.py
@@ -16,80 +16,7 @@ import application.models.skiboard as skiboard
 
 __author__ = 'liamkenny'
 
-unit_names = {
-    'size':         ['size', 'length'],
-    'nose_width':   ['nose width', 'tip width'],
-    'waist_width':  ['waist width'],
-    'tail_width':   ['tail width'],
-    'sidecut':      ['sidecut', 'sidecut radius' 'turning radius'],
-    'setback':      ['stance setback'],
-    'stance range': ['stance range'],
-    'profile':      ['bend', 'profile'],
-    'flex':         ['flex', 'stiffness'],
-    'asym':         False
-}
 
-
-# --------------------------------------------------
-# Find Best Param Name Match         F U N C T I O N
-# --------------------------------------------------
-def find_best_param_match(param):
-    match_scores = {
-        'size': 0,
-        'nose_width': 0,
-        'waist_width': 0,
-        'tail_width': 0,
-        'sidecut': 0,
-        'bend': 0,
-        'flex': 0
-    }
-
-    # Compare each possible param with given name
-    # Store highest similarity score for any unit name
-    for key in unit_names:
-        scores = []
-        for name in unit_names[key]:
-            scores.append(SequenceMatcher(None, name, param).ratio())
-
-        match_scores[key] = max(scores)
-
-    # Find the param with the highest comparison score
-    max_score = 0
-    best_match = ''
-    for key in match_scores:
-        if match_scores[key] > max_score:
-            max_score = match_scores[key]
-            best_match = key
-        
-    if max_score == 0:
-        return None
-
-    return key, {param: max_score}
-
-# --------------------------------------------------
-# Match Import Params                F U N C T I O N
-# --------------------------------------------------
-def match_params(params, units):
-
-    best_matches = {
-        'size': {},
-        'nose_width': {},
-        'waist_width': {},
-        'tail_width': {},
-        'sidecut': {},
-        'bend': {},
-        'flex': {}
-    }
-
-    # Generate list of best matches for each valid param name
-    for param in params:
-        if param:
-            key, match_score = find_best_param_match(param)
-            best_matches[key].append(match_score)
-
-        
-    # Return best matching input param names for each valid param name
-    return best_matches
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # N E W   I M P O R T                  H A N D L E R
@@ -147,66 +74,29 @@ class ImportDetailsHandler(MethodView):
 
     # -------------------------------------- P O S T
     def post(r, id):
-        from ... import app
-        raw_data = request.form['data_table']
         item = skiboard.get_item_by_id(id)
-        logging.info("Size Chart Submitted: \n{}".format(raw_data))
+        raw_input = request.form['data_table']
 
-        # Extract Data from raw table
-        sizes = 0
-        params = {}
-        param_units = {}
-        f = open("logs.txt", "a")
-        f.write("{}\nEXTRACTING...".format(datetime.now(pytz.timezone('Canada/Pacific'))))
-        for line in raw_data.split('\n'):
-            f.write("\n{}".format(line))
-            split_line = line.split(" ")
-            
-            line_name = " ".join(split_line[:-1])
-            f.write("\nName: {}".format(line_name))
+        # Prevent incomplete submission
+        if not item or not raw_input:
+            flash("We could not process your import. Please try again.")
+            return redirect("/import")
+        elif not raw_input:
+            flash("We could not process your import. Please try again.")
+            return redirect("/import/{}/".format(id))
 
-            units = str(line)[line.find('(') +1:line.find(')')]
-            f.write("\nUnits: {}".format(units))
-            
-            values = split_line[-1].split('\t')[1:]
-            for x, v in enumerate(values):
-                values[x] = v.replace('\r', '')
-            f.write("\nValues: {}".format(values))
+        logging.info("Size Chart Submitted for skiboard:{}\n{}".format(id, raw_input))
+        params, units, sizes = skiboard.extract_params_from_text(raw_input)
+        logging.info("Extracted params: {}\n\nExtracted units: {}".format(params, units))
 
-            #params.append({'key': line_name, 'unit': units, 'values': values})
-            params[line_name.replace(' ', '_')] = values
-            param_units[line_name.replace(' ', '_')] = values
+        # Normalise params
+        formatted_params = skiboard.format_params(params, units)
+        unit_options = list(skiboard.unit_names.keys())
 
-            sizes += 1
+        # Update firebase doc
         
-        f.write('\nUnformatted params:\n{}'.format(json.dumps(params)))
-        formatted_params = match_params(params, units)
-        f.write('\n\nFormatted params:\n{}\n\n'.format(json.dumps(formatted_params)))
-        f.close()
-
-        # Format completed data object
-        data = {
-            'type': item['type'],
-            'brand_name': item['brand'],
-            'model_name': item['model'],
-            'year': item['year'],
-            'params': []
-        }
-
-        # Populate data object with matched params
-        for param in formatted_params:
-            new_param = {
-                'name': None,
-                'matched': None,
-                'val_name': None,
-                'vals': []
-            }
-
-            data['params'].append(new_param)
-
-        unit_options = list(unit_names.keys())
-        
-        return render_template('imports/import_confirmation.html', page_name='import_conf', data=data, options=unit_options, sizes=sizes)
+                
+        return render_template('imports/import_confirmation.html', page_name='import_conf', skiboard=item)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # I M P O R T   C O N F                H A N D L E R
--- application/models/skiboard.py
+++ application/models/skiboard.py
@@ -3,6 +3,22 @@ from firebase_admin import firestore
 
 __author__ = 'liamkenny'
 
+unit_names = {
+    'size':         ['size', 'length'],
+    'nose_width':   ['nose width', 'tip width'],
+    'waist_width':  ['waist width'],
+    'tail_width':   ['tail width'],
+    'sidecut':      ['sidecut', 'sidecut radius' 'turning radius'],
+    'setback':      ['stance setback'],
+    'stance width': ['stance width', 'stance range'],
+    'profile':      ['bend', 'profile'],
+    'flex':         ['flex', 'stiffness'],
+    'asym':         False
+}
+
+
+
+
 # --------------------------------------------------
 # Is Duplicate                       F U N C T I O N
 # --------------------------------------------------
@@ -42,3 +58,43 @@ def get_item_by_id(id):
 
     return False
 
+# --------------------------------------------------
+# Extract Params from Text           F U N C T I O N
+# --------------------------------------------------
+def extract_params_from_text(raw_input):
+    # Initialise empty dictionaries
+    sizes = 0
+    params = {}
+    param_units = {}
+
+    # Itterate throguh each row
+    for row in raw_input.split('\n'):
+        split_row = row.split(" ")
+
+        # Extract name from row
+        row_name = " ".join(split_row[:-1])
+
+        # Extract unit from row
+        if '(' in str(row):
+            units = str(row)[row.find('(') +1:row.find(')')]
+        else:
+            units = None
+
+        # Extract values from row
+        values = split_row[-1].split('\t')[1:]
+
+        # Remove rogue characters
+        for x, v in enumerate(values):
+            values[x] = v.replace('\r', '')
+
+        # Create dict for params and units
+        params[row_name.replace(' ', '_').lower()] = values
+        param_units[row_name.replace(' ', '_').lower()] = units
+
+        sizes += 1
+        
+    return params, param_units, sizes
+
+def format_params(unformatted):
+    formatted = None
+    return formatted
